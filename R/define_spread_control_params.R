a9fd8126a699bd6a1f292c726d67ad2172e3bcae
Rachel Woodside <rachel.woodside3@gmail.com>
2024-10-23 14:24:20 -0300

    Incorporate hydroepix-specific params into param-defining functions

--- R/define_spread_control_params.R
+++ R/define_spread_control_params.R
@@ -1,17 +1,19 @@
 define_spread_control_params <-
   function(new_infection_functions = c("BCSpread()", "DBinf()"),
-           t_stochastic = FALSE,
-           rf_stochastic = FALSE,
+           t_stochastic = FALSE, # TODO: options are stochastic or just 1 - rework to better represent this choice?
+           rf_stochastic = FALSE, # TODO: options are binomial chain model or Reed-Frost model - rework to better represent this choice?
            index_herd_function = "selectIndexHerd",
-           index_herd_select = list(herd_type = 1:18),
+           index_herd_select = list(herd_type = 1:18), # TODO: argument provided to index_herd_function - better way to represent or name?
            index_direct = FALSE,
            first_det_para_1 = 0.00255,
-           first_det_para_2 = 0.00255,
-           subclinical_infection_risk = 0.1,
+           first_det_para_2 = 0.00255, # TODO: How are these distinct? Are they both necessary?
            case_fatality = 0.89,
            days_dead_infectious = 2,
            dead_impact = 1,
-           impact_dead_time = 1 #??
+           impact_dead_time = 1, #??
+           farm_to_farm = 0.42,
+           cage_to_cage = 0.052,
+           vaccine_efficacy = 0
            ) {
 
   }
--- R/define_surveillance_control_params.R
+++ R/define_surveillance_control_params.R
@@ -1,10 +1,12 @@
 define_surveillance_control_params <-
-  function(delay_times = expression(round(rpert(n, 1, 2, 4))), # TODO: Can we reformat this?
+  function(delay_times = expression(round(rpert(n, 1, 2, 4))), # TODO: Can we reformat this default? Do we need to?
            prob_diagnosis = 1, # TODO: reconsider variable naming?
            mortality_increase_1 = 2,
            mortality_increase_2 = 2, # TODO: how are these distinct? are they both necessary?
+           time_between_visits_within_zone = 4, # TODO: renaming of ZSurVisit - OK?
            protection_zone_duration = 50,
-           past_days_for_dead_animal_surveillance = 7, # TODO: variable naming
+           past_days_for_dead_animal_surveillance = 7, # TODO: renaming of DaysSurDead - shorten?,
+           daily_farm_survey_limit = 2, # TODO: renaming of CapSurvey - OK?
            output_surveillance_details = FALSE) {
 
   }
