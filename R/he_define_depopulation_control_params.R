83b80da9190899923c5fcf112bb3e73cb2eb9b6f
Rachel Woodside <rachel.woodside3@gmail.com>
2024-11-18 15:30:11 -0400

    add tests and docs for depopulation control definition function

--- R/he_define_depopulation_control_params.R
+++ R/he_define_depopulation_control_params.R
@@ -1,3 +1,13 @@
+#' Define simulation variables related to depopulation control
+#'
+#' @param environment environment in which simulation variables are stored and managed
+#' @param culling_capacity vector of integer values indicating the number of individuals which can be culled??
+#' @param cull_types vector of indices to indicate which species can be culled??
+#' @param cull_farm_if_cage_infection boolean indicating whether the entire farm should be culled if an infection is detected in a cage
+#'
+#' @return NA
+#' @export
+#'
 he_define_depopulation_control_params <-
   function(environment,
            culling_capacity = c(20000),
