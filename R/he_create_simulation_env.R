a575f6a0bf185558d06321234ad4297b6e047d92
Rachel Woodside <rachel.woodside3@gmail.com>
2024-12-04 15:15:38 -0400

    Fix simulation creation function and test renaming

--- R/he_create_simulation_env.R
+++ R/he_create_simulation_env.R
@@ -1,4 +1,4 @@
-he_initialize_simulation_env <- function() {
+he_create_simulation_env <- function() {
   # This function ensures the empty environment is the parent
   # This ensures the variable search path is limited to this environment
   rlang::new_environment()
