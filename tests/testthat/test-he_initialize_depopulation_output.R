ee14ba21f76d73169445052a768600e50bbae3d8
Rachel Woodside <rachel.woodside3@gmail.com>
2024-12-13 16:22:12 -0400

    WIP updating test with correct use of temp directories

--- tests/testthat/test-he_initialize_depopulation_output.R
+++ tests/testthat/test-he_initialize_depopulation_output.R
@@ -1,29 +1,56 @@
 test_that("check initialized default values for depopulation output file", {
   test_environment <- rlang::new_environment()
-  filepath <- withr::local_tempfile(pattern = "depopulated_farms",
-                                    fileext = ".txt")
-  he_initialize_depopulation_output(test_environment, filepath)
-  expect_equal(test_environment$depopulation_output_file_name,
-               "depopulated_farms.txt")
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "depopulated_farms.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      expect_true(file.exists(test_environment$filepath))
+      expect_equal(test_environment$depopulation_output_file_name,
+                   "depopulated_farms.txt")
+    }
+  )
 })
 
 test_that("check initialized non-default values for depopulation output file", {
   test_environment <- rlang::new_environment()
   test_environment$run_id <- 1
-  filepath <- withr::local_tempfile(pattern = "depopulated_farms",
-                                    fileext = ".txt")
-  he_initialize_depopulation_output(test_environment, filepath)
-  expect_equal(test_environment$depopulation_output_file_name,
-               "1-depopulated_farms.txt")
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "depopulated_farms.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      he_initialize_depopulation_output(test_environment, filepath)
+      expect_equal(test_environment$depopulation_output_file_name,
+                   "1-depopulated_farms.txt")
+    }
+  )
 })
 
 test_that("check initialized values in depopulation matrix", {
   test_environment <- rlang::new_environment()
-  filepath <- withr::local_tempfile(pattern = "depopulated_farms",
-                                    fileext = ".txt")
-  he_initialize_depopulation_output(test_environment, filepath)
-  expect_equal(test_environment$depopulation_matrix_output,
-               matrix(numeric(0), ncol = 3))
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "depopulated_farms.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      he_initialize_depopulation_output(test_environment, filepath)
+      expect_equal(test_environment$depopulation_matrix_output,
+                   matrix(numeric(0), ncol = 3))
+    }
+  )
 })
 
 # TODO: Tests to see whether the file is written?
--- tests/testthat/test-he_initialize_infected_netpen_output.R
+++ tests/testthat/test-he_initialize_infected_netpen_output.R
@@ -1,29 +1,56 @@
 test_that("check initialized default values for infected netpen output file", {
   test_environment <- rlang::new_environment()
-  filepath <- withr::local_tempfile(pattern = "infected_netpens",
-                                    fileext = ".txt")
-  he_initialize_infected_netpen_output(test_environment, filepath)
-  expect_equal(test_environment$infected_output_file_name,
-               "infected_netpens.txt")
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "infected_netpens.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      he_initialize_infected_netpen_output(test_environment, filepath)
+      expect_equal(test_environment$infected_output_file_name,
+                   "infected_netpens.txt")
+    }
+  )
 })
 
 test_that("check initialized non-default values for infected netpen output file", {
   test_environment <- rlang::new_environment()
   test_environment$run_id <- 1
-  filepath <- withr::local_tempfile(pattern = "infected_netpens",
-                                    fileext = ".txt")
-  he_initialize_infected_netpen_output(test_environment, filepath)
-  expect_equal(test_environment$infected_output_file_name,
-               "1-infected_netpens.txt")
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "infected_netpens.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      he_initialize_infected_netpen_output(test_environment, filepath)
+      expect_equal(test_environment$infected_output_file_name,
+                   "1-infected_netpens.txt")
+    }
+  )
 })
 
 test_that("check initialized values in infected netpen matrix", {
   test_environment <- rlang::new_environment()
-  filepath <- withr::local_tempfile(pattern = "infected_netpens",
-                                    fileext = ".txt")
-  he_initialize_infected_netpen_output(test_environment, filepath)
-  expect_equal(test_environment$infected_netpens,
-               matrix(numeric(0), ncol = 10))
+  test_dir <- "temp_test_dir"
+  withr::local_tempfile(
+    test_dir,
+    {
+      test_environment$filepath <-
+        file.path(test_dir, "infected_netpens.txt")
+      expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                        test_environment$filepath,
+                                                        append = FALSE))
+      he_initialize_infected_netpen_output(test_environment, filepath)
+      expect_equal(test_environment$infected_netpens,
+                   matrix(numeric(0), ncol = 10))
+    }
+  )
 })
 
 # TODO: Tests to see whether the file is written?
