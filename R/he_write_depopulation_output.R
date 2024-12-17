7e7ec450bcab3a92b60c75077f9e390242a3f019
rachelwoodside <rachel.woodside3@gmail.com>
2024-12-17 16:50:54 -0400

    Correct use of withr in depopulation writing function tests, add error checks to depop writing function

--- R/he_write_depopulation_output.R
+++ R/he_write_depopulation_output.R
@@ -7,10 +7,25 @@
 #' @return NA
 #' @export
 #'
-he_write_depopulation_output <- function(environment, append = FALSE) {
+he_write_depopulation_output <- function(environment, output_dir = "", append = FALSE) {
+  if (is.null(environment$depopulation_matrix_output)) {
+    stop("No depopulation matrix initialized. Run
+          `he_initialize_depopulation_output()` to initialize before attempting
+          to write output.")
+  }
+
+  if (is.null(environment$depopulation_output_file_name)) {
+    stop("No depopulation output file name initialized.
+          Run `he_initialize_depopulation_output()` to assign
+          a custom or default name to the output file.")
+  }
+
+  message(paste0("Writing to ",
+                 file.path(output_dir,
+                           environment$depopulation_output_file_name)))
   write.table(
     environment$depopulation_matrix_output,
-    paste0(environment$filepath, environment$depopulation_output_file_name),
+    file.path(output_dir, environment$depopulation_output_file_name),
     append = append,
     col.names = FALSE,
     row.names = FALSE
--- tests/testthat/test-he_write_depopulation_output.R
+++ tests/testthat/test-he_write_depopulation_output.R
@@ -1,23 +1,46 @@
-test_that("depopulation output file is created", {
+test_that("depopulation output file generates error for missing depopulation matrix", {
+  # Create test directory and environment
+  temp_test_dir <- "temp_test_dir"
+  withr::local_tempdir(pattern = temp_test_dir)
   test_environment <- rlang::new_environment()
-  test_dir <- "temp_test_dir"
+  # Check for error
+  expect_error(he_write_depopulation_output(test_environment,
+                                            output_dir = temp_test_dir),
+               regexp = "No depopulation matrix initialized.")
+})
+
+test_that("depopulation output file generates error for missing depopulation file name", {
+  # Create test directory and environment
+  temp_test_dir <- "temp_test_dir"
+  withr::local_tempdir(pattern = temp_test_dir)
+  test_environment <- rlang::new_environment()
+  # Populate environment variables
+  test_environment$depopulation_matrix_output <- matrix(numeric(0), ncol = 3)
+  expect_error(he_write_depopulation_output(test_environment,
+                                            output_dir = temp_test_dir),
+               regexp = "No depopulation output file name initialized.")
+})
+
+test_that("depopulation output file is created in expected directory", {
+  test_environment <- rlang::new_environment()
+  temp_test_dir <- "temp_test_dir"
+  test_environment$depopulation_matrix_output <- matrix(numeric(0), ncol = 3)
+  test_environment$depopulation_output_file_name <- "depopulated_farms.txt"
+  expected_filepath <- file.path(test_dir,
+                                 test_environment$depopulation_output_file_name)
   withr::local_tempfile(
-    test_dir,
     {
-      test_environment$filepath <-
-        file.path(test_dir, "depopulated_farms.txt")
-      expect_no_error(he_write_depopulation_output(test_environment,
-                                                   append = FALSE))
-      expect_true(file.exists(test_environment$filepath))
-    }
+      he_write_depopulation_output(test_environment, test_dir)
+      expect_true(file.exists(expected_filepath))
+      actual_data <- read.table(expected_filepath)
+      expect_equal(actual_data, test_environment$depopulation_matrix_output)
+    },
+    tmpdir = temp_test_dir
   )
 })
-#
-# test_that("depopulation file is appended", {
-#   test_environment <- rlang::new_environment()
-#   filepath <- withr::local_tempfile(pattern = "depopulated_farms",
-#                                     fileext = ".txt")
-#   he_initialize_depopulation_output(test_environment, filepath)
-#   expect_equal(test_environment$depopulation_output_file_name,
-#                "depopulated_farms.txt")
-# })
+
+# TODO: Test that output file data is correctly written
+
+# TODO: Check file output is overwritten on another call to the function
+
+# TODO: Test that output file data is correctly appended, if append option selected
