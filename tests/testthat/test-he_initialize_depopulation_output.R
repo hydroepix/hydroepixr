6fc6bdf526e892ea4207ba2cb031b7df2050ff9f
Rachel Woodside <rachel.woodside3@gmail.com>
2025-01-06 12:17:32 -0400

    Rework depop init func and tests to match depop writing func
    Updated with custom output test setup to work without withr

--- tests/testthat/test-he_initialize_depopulation_output.R
+++ tests/testthat/test-he_initialize_depopulation_output.R
@@ -1,54 +1,43 @@
 test_that("check initialized default values for depopulation output file", {
+  # Create test directory and test simulation environment
   test_environment <- rlang::new_environment()
-  test_dir <- "temp_test_dir"
-  withr::local_tempfile(
-    test_dir,
-    {
-      test_environment$filepath <-
-        file.path(test_dir, "depopulated_farms.txt")
-      expect_no_error(he_initialize_depopulation_output(test_environment,
-                                                        test_environment$filepath))
-      expect_true(file.exists(test_environment$filepath))
-      expect_equal(test_environment$depopulation_output_file_name,
-                   "depopulated_farms.txt")
-    }
-  )
+  test_environment$filepath <- output_test_setup()
+  # Check function runs without errors
+  expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                    test_environment$filepath))
+  # Check depopulation output file name is initialized to the default value
+  expect_equal(test_environment$depopulation_output_file_name,
+               "depopulated_farms.txt")
+  # Check if file has been written according to file name
+  expect_true(file.exists(file.path(test_environment$filepath, "depopulated_farms.txt")))
 })
 
 test_that("check initialized non-default values for depopulation output file", {
+  # Create test directory and test simulation environment
   test_environment <- rlang::new_environment()
+  test_environment$filepath <- output_test_setup()
   test_environment$run_id <- 1
-  test_dir <- "temp_test_dir"
-  withr::local_tempfile(
-    test_dir,
-    {
-      test_environment$filepath <-
-        file.path(test_dir, "depopulated_farms.txt")
-      expect_no_error(he_initialize_depopulation_output(test_environment,
-                                                        test_environment$filepath))
-      he_initialize_depopulation_output(test_environment, filepath)
-      expect_equal(test_environment$depopulation_output_file_name,
-                   "1-depopulated_farms.txt")
-    }
-  )
+  test_environment$depopulation_output_file_name <- "depopulation_output.txt"
+  # Check function runs without errors
+  expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                    test_environment$filepath,
+                                                    test_environment$depopulation_output_file_name))
+  # Check depopulation output file name is initialized to the custom value
+  expect_equal(test_environment$depopulation_output_file_name,
+               "1-depopulation_output.txt")
+  # Check if file has been written according to file name
+  expect_true(file.exists(file.path(test_environment$filepath, "1-depopulation_output.txt")))
 })
 
 test_that("check initialized values in depopulation matrix", {
+  # Create test directory and test simulation environment
   test_environment <- rlang::new_environment()
-  test_dir <- "temp_test_dir"
-  withr::local_tempfile(
-    test_dir,
-    {
-      test_environment$filepath <-
-        file.path(test_dir, "depopulated_farms.txt")
-      expect_no_error(he_initialize_depopulation_output(test_environment,
-                                                        test_environment$filepath))
-      he_initialize_depopulation_output(test_environment, filepath)
-      expect_equal(test_environment$depopulation_matrix_output,
-                   matrix(numeric(0), ncol = 3))
-    }
-  )
-})
+  test_environment$filepath <- output_test_setup()
 
-# TODO: Tests to see whether the file is written?
-# TODO: Tests to see whether the values in the file are as expected?
+  # Check function runs without errors
+  expect_no_error(he_initialize_depopulation_output(test_environment,
+                                                    test_environment$filepath))
+  # Check depopulation matrix is initialized to default values
+  expect_equal(test_environment$depopulation_matrix_output,
+               matrix(numeric(0), ncol = 3))
+})
