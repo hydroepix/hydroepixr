f17e321457b80ad0b245acd3befef182d8918b9c
Rachel Woodside <rachel.woodside3@gmail.com>
2025-01-22 16:08:29 -0400

    Seaway distance calculation func and test

--- tests/testthat/test-he_generate_seaway_dist_prob.R
+++ tests/testthat/test-he_generate_seaway_dist_prob.R
@@ -1,4 +1,4 @@
-test_that("seaway distance access distance matrix values correctly", {
+test_that("seaway distance values are calculated correctly", {
   test_dist_mat <- matrix(c(0, 2000, 7000, 2000, 0, 5000, 7000, 5000, 0),
                           nrow = 3,
                           ncol = 3)
@@ -6,11 +6,16 @@ test_that("seaway distance access distance matrix values correctly", {
   test_dist <- test_dist_mat[test_farm_id,]
   test_farm_to_farm <- 0.5
   test_vaccine_efficacy <- 0
-  expected <- c(0.033333333, 0.012262648, 0.001006579)
+  expected <- c(0.0333333333333333,
+                0.0122626480390481,
+                0.00100657944741062)
   actual <- he_generate_seaway_dist_prob(test_dist,
                                          test_farm_to_farm,
                                          test_vaccine_efficacy)
+  # Check for only numeric values
+  expect_true(all(sapply(actual, is.numeric)))
   # Check the length returned matches the length inputted
   expect_length(actual, length(test_dist))
-  expect_setequal(actual, expected)
+  # Check the set of values output is equal
+  expect_equal(actual, expected)
 })
