c03b62e0d0c1d6a951777eaa28ba105798ce71ce
Rachel Woodside <rachel.woodside3@gmail.com>
2025-02-10 13:50:20 -0400

    Remove non-basic functionality variables from simulation env

--- R/he_initialize_simulation_env.R
+++ R/he_initialize_simulation_env.R
@@ -8,55 +8,20 @@
 #' @export
 #' @importFrom stats runif
 #'
-he_initialize_simulation_env <- function(environment, num_farms, farm_info) {
-  # gDaysUntilBaseline - determine the day the first detection will happen
-  environment$days_until_baseline_mort <- 0
+he_initialize_simulation_env <- function(environment, num_farms, farm_info, species_info) {
+  # Initialize simulation-level farm information variables
+  farm_info$susceptible_again <- rep(0, num_farms)
+  farm_info$survived <- rep(0, num_farms)
+  farm_info$infectiousness <- rep(0, num_farms)
+  farm_info$infection_mode <- rep(0, num_farms)
 
-  # TotNumQueue - output variable to calculate the number of "herds" queueing
-  # for surveillance per day
-  # does not affect anything (TH) ?
-  environment$total_num_queued <- 0
-
-  # Initialize waiting periods and transmission probabilities
-  farm_info$tagged_duration <- rep(0, num_farms) # aHerd$taggedDur
-  # New additions TH
-  farm_info$detected_tagged_dep <- rep(FALSE, num_farms) #aHerd$DetTaggedDep
-  farm_info$time_visited <- rep(0, num_farms) #aHerd$timeVisited
-  farm_info$immune_time <- rep(0, num_farms) #aHerd$immuneTime
-  farm_info$time_culled <- rep(0, num_farms) #aHerd$timeCulled
-  farm_info$to_surv_dead <- rep(0, num_farms) #aHerd$toSurvDead
-  farm_info$dead_sample_test <- rep(0, num_farms) #aHerd$DeadSampTest
-  farm_info$sampled_dead <- rep(0, num_farms) #aHerd$SampledDead
-  farm_info$diagnosed_surveyed_dead <- rep(FALSE, num_farms) #aHerd$DiagSurvDead
-  farm_info$sub_dead_sample <- rep(0, num_farms) #aHerd$SubDeadSamp
-  farm_info$susceptible_again <- rep(0, num_farms) #aHerd$SusAgain
-  farm_info$survived <- rep(0, num_farms) #aHerd$Survived
-  farm_info$in_surveillance_zone <- rep(0, num_farms) #aHerd$inSurZone
-
-  # New variables TA and DP
-  environment$farm_info$infectiousness <- rep(0, num_farms) #aHerd$Infectiousness
-  environment$farm_info$infection_mode <- rep(0, num_farms) #aHerd$infMode
-  environment$farm_info$netpen_size_variable <- farm_info$netpen_size #aHerd$CageSizeVar
-  environment$farm_info$netpen_size_cull <- farm_info$netpen_size #aHerd$CageSizeCull
-  environment$farm_info$diagnosis_mode <- rep(0, num_farms) #aHerd$DiagMode
-  environment$farm_info$time_to_first_surveillance_visit <- rep(0, num_farms) #aHerd$timeToFSV
-  environment$farm_info$time_to_subsequent_surveillance_visit <- rep(0, num_farms) #aHerd$timeToSSV
-  environment$farm_infO$time_to_asv <- rep(0, num_farms) #aHerd$timeToASV
-  environment$farm_infO$farm_diagnosis <- rep(FALSE, num_farms) #aHerd$FarmDiag
-  # number of samples for surveillance - to be checked by DP
-  environment$farm_info$number_of_samples <- round(rtriang(num_farms, 5, 10, 20)) #aHerd$NumSamp
-  environment$farm_info$number_of_samples_surveyed <- rep(0, num_farms) #aHerd$NumSamSurv
-  environment$farm_info$farm_size_surveyed <- rep(0, num_farms) #aHerd$herdSizeSurv
-  environment$farm_info$visit_count <- rep(0, num_farms) #aHerd$visitCount
-  farm_a_s_visit <- round(runif(unique(environment$farm_info$farm_id)), 1, 90)
-  environment$farm_info$time_to_asv <- farm_a_s_visit[environment$farm_info$farm_id]
-
+  # TODO: Check if this overlaps with he_initialize_farm_info?
   # iterate over species to set waiting periods and transmission probabilities
   # by species
   for (id in species_info$species_id) {
-    farm_index <- environment$farm_info$species_id == id
+    farm_index <- farm_info$species_id == id
     num_type <- sum(farm_index)
-    species_index <- environment$species_info$species_id
+    species_index <- species_info$species_id
     # intra-farm interaction rate
     # TODO
     # Assign k value to farm based on k value of species
@@ -86,7 +51,8 @@ he_initialize_simulation_env <- function(environment, num_farms, farm_info) {
     farm_info$status[environment$index_farm]
     farm_info$time_infected[environment$index_farm] <- 0
 
-    # TODO: Confirm OK to skip "step-in file" section
+    # TODO: infect index farm with number of fish according to type of
+    # initial infection
     if (index_direct) {
       # TODO: construct an infected farm
       # aInfHerd$addInf(indexHerd,
@@ -105,9 +71,6 @@ he_initialize_simulation_env <- function(environment, num_farms, farm_info) {
       #                 0)
     }
   }
-  # TODO: What are these?
-  # environment$vaccinated_today <- 0
-  # environment$lock <- FALSE
 
   # TODO: Review these variables - what are these bay management area vars for?
   temp_bay_management <- round(runif(unique(farm_info$bay_management_id)))
