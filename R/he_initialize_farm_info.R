c544b619fbdd4bd6b0396d7b29e09076f6623fb8
rachelwoodside <rachel.woodside3@gmail.com>
2025-02-20 14:20:19 -0400

    Modify farm info init func for new species info format, add rpert distribution calc

--- R/he_initialize_farm_info.R
+++ R/he_initialize_farm_info.R
@@ -11,16 +11,50 @@ he_initialize_farm_info <- function(farm_info, species_info) {
   species_info_subset <-
     subset(species_info,
            subset = TRUE, # this appears to be necessary for a df for some reason
-           select = c(species_id, within_netpen_transmission, rel_susceptibility))
+           select = c(
+             "species_id",
+             "within_netpen_transmission_min",
+             "within_netpen_transmission_mode",
+             "within_netpen_transmission_max",
+             "rel_susceptibility"
+           ))
+
   # left outer join
   initialized_farm_info <- merge(farm_info,
                                  species_info_subset,
                                  all.x = TRUE)
-  if (any(sapply(initialized_farm_info$within_netpen_transmission, is.na)) |
-      any(sapply(initialized_farm_info$rel_susceptibility, is.na))) {
-    stop("Missing species info for species_id in farm_info.
+  missing_species_info <- unlist(purrr::map(
+    initialized_farm_info[c(
+      "within_netpen_transmission_min",
+      "within_netpen_transmission_mode",
+      "within_netpen_transmission_max",
+      "rel_susceptibility"
+    )],
+    \(x) any(is.na(x)))
+    )
+
+  # Check for any species_id in farm_info without corresponding details in
+  # species_info
+  if (any(missing_species_info)) {
+    stop("Missing species info for a species in farm_info.
          Please check files to ensure all species_id in the farm_info file have
          corresponding information in the species_info file.")
   }
-  initialized_farm_info
+
+  # Calculate random variates for within netpen transmission
+  # from triangular distribution
+  initialized_farm_info$within_netpen_transmission <- purrr::pmap(
+    list(
+      1,
+      initialized_farm_info$within_netpen_transmission_min,
+      initialized_farm_info$within_netpen_transmission_mode,
+      initialized_farm_info$within_netpen_transmission_max
+    ),
+    he_rpert
+  )
+  # Remove intermediate columns
+  subset(initialized_farm_info,
+         select = -c(within_netpen_transmission_min,
+                     within_netpen_transmission_mode,
+                     within_netpen_transmission_max))
 }
--- tests/testthat/test-he_initialize_farm_info.R
+++ tests/testthat/test-he_initialize_farm_info.R
@@ -1,16 +1,19 @@
 test_that("farm info values are initialized correctly", {
   farm_info <- readRDS(paste0(test_data_filepath, "/raw_farm_info_bay_x.rds"))
-  species_info <- readRDS(paste0(test_data_filepath, "/raw_species_info_bay_x.rds"))
+  species_info <- readRDS(paste0(test_data_filepath, "/parsed_species_info_bay_x.rds"))
   initialized_farm_info <- he_initialize_farm_info(farm_info, species_info)
   expect_true(all(
     initialized_farm_info$rel_susceptibility == 1))
-  expect_true(all(
-    initialized_farm_info$within_netpen_transmission == "rpert(n,0.14,0.4,0.8)"))
+  # expect_true(all(
+  #   initialized_farm_info$within_netpen_transmission == "rpert(n,0.14,0.4,0.8)"))
 })
 
 test_that("farm info species with missing species info causes an error", {
   farm_info <- readRDS(paste0(test_data_filepath,
                               "/raw_farm_info_bay_x_species_id_mismatch.rds"))
-  species_info <- readRDS(paste0(test_data_filepath, "/raw_species_info_bay_x.rds"))
-  expect_error(he_initialize_farm_info(farm_info, species_info))
+  species_info <- readRDS(paste0(test_data_filepath, "/parsed_species_info_bay_x.rds"))
+  expect_error(he_initialize_farm_info(farm_info, species_info),
+               regex = "Missing species info for a species in farm_info.
+         Please check files to ensure all species_id in the farm_info file have
+         corresponding information in the species_info file.")
 })
