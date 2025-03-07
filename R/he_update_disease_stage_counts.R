fc913d45b01ac0eb17ffd1d4aec916b96fcbcd07
rachelwoodside <rachel.woodside3@gmail.com>
2025-03-07 12:11:41 -0400

    Detailed input testing for he_update_disease_stage_counts

--- R/he_update_disease_stage_counts.R
+++ R/he_update_disease_stage_counts.R
@@ -14,35 +14,72 @@
 he_update_disease_stage_counts <- function(disease_stage_counts,
                                            disease_stage_duration_matrices,
                                            num_newly_infected) {
+  # This function assumes at least three disease stages because of the call to
+  # he_calculate_net_change_in_disease_stage_count
+  if (ncol(disease_stage_counts) < 3) {
+    stop("At least three disease stage counts are required in order to
+         calculate transition between a beginning, intermediate, and final stage
+         of disease. Please include at least three disease stage count columns.")
+  }
+
+  # The number of infected netpens (rows) should be consistent across disease
+  # stage duration matrices
+  num_rows_disease_stage_duration_matrices <-
+    sapply(disease_stage_duration_matrices, nrow)
+
+  if (length(unique(num_rows_disease_stage_duration_matrices)) > 1) {
+    stop("Inconsistent number of rows in disease stage duration matrices.
+         Different matrices have numbers of rows as follows: ",
+         paste(num_rows_disease_stage_duration_matrices, collapse = ", "))
+  }
+
+  # The number of netpens should be consistent across provided arguments:
+  # Length of newly infected
+  # Number of rows in disease stage counts
+  # Number of rows in duration matrices
+  if (nrow(disease_stage_counts) != length(num_newly_infected)) {
+    stop("Mismatched number of infected netpens (rows) between disease stage
+         counts and number of newly infected animals. The number of newly
+         infected animal values should be equal to the number of rows of
+         disease stage counts.\n
+         Number of rows in disease stage counts: ",
+         nrow(disease_stage_counts), "\n
+         Number of newly infected animal values: ",
+         length(num_newly_infected))
+  }
+  if (!all(nrow(disease_stage_counts) ==
+           num_rows_disease_stage_duration_matrices)) {
+    stop("Mismatched number of infected netpens (rows) between disease stage
+         counts and disease stage duration matrices. All duration matrices and
+         the disease stage counts should all have the same number of rows.\n
+         Number of rows in each disease stage duration matrix: ",
+         paste(num_rows_disease_stage_duration_matrices, collapse = ", "),
+         "\n
+         Number of rows in disease stage counts: ",
+         nrow(disease_stage_counts)
+         )
+  }
+
   # There should be exactly two more disease stage counts than there are
   # disease stage matrices, which accounts for the starting stage (susceptible)
   # and the ending stage (immune, which includes recovered and dead)
-  if (ncol(disease_stage_counts) ==
-      length(disease_stage_duration_matrices) + 2) {
-    # TODO: Other checks to make:
-    # 1. At least 3 disease stage counts because we assume a starting,
-    # intermediate, and ending  disease stage at minimum
-    # 2. Length of newly infected should equal the number of rows in disease
-    # stage counts and number of rows in duration matrices
-    # (i.e. number of netpens matches up)
-
-    # Store the net change for each disease stage, based on the number of newly
-    # infected and/or the disease stage duration matrices
-    net_change <-
-      he_calculate_net_change_in_disease_stage_count(
-        disease_stage_duration_matrices,
-        num_newly_infected
-      )
-    disease_stage_counts + net_change
-  } else {
+  if (ncol(disease_stage_counts) !=
+           length(disease_stage_duration_matrices) + 2) {
     stop("Mismatched number of disease stage counts and disease stage duration
          matrices. The number of disease stage duration matrices should be two
          less than the number of disease stage counts. \n
          Number of disease stage duration matrices: ",
          length(disease_stage_duration_matrices), "\n
          Number of disease stage counts: ",
-         ncol(disease_stage_counts) )
+         ncol(disease_stage_counts)
+         )
   }
 
+    net_change <-
+      he_calculate_net_change_in_disease_stage_count(
+        disease_stage_duration_matrices,
+        num_newly_infected
+      )
+    disease_stage_counts + net_change
 }
 
--- tests/testthat/test-he_update_disease_stage_counts.R
+++ tests/testthat/test-he_update_disease_stage_counts.R
@@ -72,29 +72,179 @@ test_that("disease stage count values are updated correctly for multiple netpens
                expected_updated_disease_stage_counts)
 })
 
-test_that("too few disease stage counts for the number of disease stage duration
-          matrices generates an error", {
-
-})
-
-test_that("too many disease stage counts for the number of disease stage duration
-          matrices generates an error", {
-
+test_that("providing fewer than 3 columns for disease stage counts generates an
+          error", {
+  test_disease_stage_counts <- data.frame(
+    susceptible = 25000,
+    immune = 10000
+  )
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(25, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE),
+    matrix(c(10, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE),
+    matrix(c(5, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE)
+  )
+  test_num_newly_infected <- 1000
+  expect_error(
+    he_update_disease_stage_counts(test_disease_stage_counts,
+                                   test_disease_stage_duration_matrices,
+                                   test_num_newly_infected),
+    regexp = "At least three disease stage counts are required in order to
+         calculate transition between a beginning, intermediate, and final stage
+         of disease. Please include at least three disease stage count columns."
+    )
 })
 
 test_that("error generated when number of netpens recorded in disease stage
           counts does not match number of values provided for newly infected", {
-
+  test_disease_stage_counts <- data.frame(
+    susceptible = 25000,
+    latent = 10000,
+    subclinical = 10000,
+    clinical = 5000,
+    immune = 0
+  )
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(25, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE),
+    matrix(c(10, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE),
+    matrix(c(5, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE)
+  )
+  test_num_newly_infected <- c(1000, 1000)
+  expect_error(
+    he_update_disease_stage_counts(test_disease_stage_counts,
+                                   test_disease_stage_duration_matrices,
+                                   test_num_newly_infected),
+    regexp = "Mismatched number of infected netpens (rows) between disease stage
+         counts and number of newly infected animals. The number of newly
+         infected animal values should be equal to the number of rows of
+         disease stage counts.\n
+         Number of rows in disease stage counts: 1\n
+         Number of newly infected animal values: 2",
+    fixed = TRUE
+  )
 })
 
 test_that("error generated when number of netpens recorded in disease stage
-          duration matrices does not match number of netpens recorded in disease
+          duration matrices is not consistent across matrices", {
+  test_disease_stage_counts <- data.frame(
+    susceptible = 25000,
+    latent = 10000,
+    subclinical = 10000,
+    clinical = 5000,
+    immune = 0
+  )
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(25, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE),
+    matrix(c(10, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE),
+    matrix(c(5, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE)
+  )
+  test_num_newly_infected <- c(1000, 1000)
+  expect_error(
+    he_update_disease_stage_counts(test_disease_stage_counts,
+                                   test_disease_stage_duration_matrices,
+                                   test_num_newly_infected),
+    regexp = "Inconsistent number of rows in disease stage duration matrices.
+         Different matrices have numbers of rows as follows: 2, 1, 1",
+    fixed = TRUE
+  )
+})
+
+test_that("error generated when number of netpens recorded in disease stage
+          counts does not match number of netpens recorded in disease
           stage duration matrices", {
-
+  test_disease_stage_counts <- data.frame(
+    susceptible = 25000,
+    latent = 10000,
+    subclinical = 10000,
+    clinical = 5000,
+    immune = 0
+  )
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(50, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE),
+    matrix(c(25, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE),
+    matrix(c(10, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE)
+  )
+  test_num_newly_infected <- 1000
+  expect_error(
+    he_update_disease_stage_counts(test_disease_stage_counts,
+                                   test_disease_stage_duration_matrices,
+                                   test_num_newly_infected),
+    regexp = "Mismatched number of infected netpens (rows) between disease stage
+         counts and disease stage duration matrices. All duration matrices and
+         the disease stage counts should all have the same number of rows.\n
+         Number of rows in each disease stage duration matrix: 2, 2, 2\n
+         Number of rows in disease stage counts: 1",
+    fixed = TRUE
+  )
 })
 
-test_that("error generated when number of netpens recorded in disease stage
-          duration matrices does not match number of values provided for newly
-          infected", {
+test_that("too few disease stage counts for the number of disease stage duration
+          matrices generates an error", {
+    test_disease_stage_counts <- data.frame(
+      susceptible = 25000,
+      latent = 10000,
+      clinical = 5000,
+      immune = 0
+    )
+    test_disease_stage_duration_matrices <- list(
+      matrix(c(50, 0, 0, 0, 0, 0, 0, 0),
+             ncol = 8,
+             byrow = TRUE),
+      matrix(c(25, 0, 0, 0, 0, 0, 0, 0),
+             ncol = 8,
+             byrow = TRUE),
+      matrix(c(10, 0, 0, 0, 0, 0, 0, 0),
+             ncol = 8,
+             byrow = TRUE)
+    )
+    test_num_newly_infected <- 1000
+    expect_error(
+      he_update_disease_stage_counts(test_disease_stage_counts,
+                                     test_disease_stage_duration_matrices,
+                                     test_num_newly_infected),
+      regexp = "Mismatched number of disease stage counts and disease stage duration
+         matrices. The number of disease stage duration matrices should be two
+         less than the number of disease stage counts. \n
+         Number of disease stage duration matrices: 3\n
+         Number of disease stage counts: 4",
+      fixed = TRUE
+    )
+})
 
+test_that("too many disease stage counts for the number of disease stage duration
+          matrices generates an error", {
+  test_disease_stage_counts <- data.frame(
+    susceptible = 25000,
+    latent = 10000,
+    subclinical = 10000,
+    clinical = 5000,
+    immune = 0
+  )
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(50, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE),
+    matrix(c(25, 0, 0, 0, 0, 0, 0, 0),
+           ncol = 8,
+           byrow = TRUE)
+  )
+  test_num_newly_infected <- 1000
+  expect_error(
+    he_update_disease_stage_counts(test_disease_stage_counts,
+                                   test_disease_stage_duration_matrices,
+                                   test_num_newly_infected),
+    regexp = "Mismatched number of disease stage counts and disease stage duration
+         matrices. The number of disease stage duration matrices should be two
+         less than the number of disease stage counts. \n
+         Number of disease stage duration matrices: 2\n
+         Number of disease stage counts: 5",
+    fixed = TRUE
+  )
 })
