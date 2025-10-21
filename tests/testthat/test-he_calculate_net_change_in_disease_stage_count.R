89eefff0c8ec9c4f67437eed73aed64441f49729
rachelwoodside <rachel.woodside3@gmail.com>
2025-10-21 15:37:14 -0300

    Add unequal subclinical-clinical split test

--- tests/testthat/test-he_calculate_net_change_in_disease_stage_count.R
+++ tests/testthat/test-he_calculate_net_change_in_disease_stage_count.R
@@ -21,6 +21,30 @@ test_that("net change values are calculated correctly for a single netpen", {
   expect_equal(test_net_change, expected_net_change)
 })
 
+test_that("net change values are calculated correctly for a single netpen with
+          unequal subclinical-clinical split", {
+  test_disease_stage_duration_matrices <- list(
+    matrix(c(50, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE), # latent
+    matrix(c(10, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE), # subclinical
+    matrix(c(5, 0, 0, 0, 0, 0, 0, 0), ncol = 8, byrow = TRUE) # clinical
+  )
+  test_n_newly_infected <- 1000
+  test_clinically_infected_prop <- 0.77
+  test_net_change <-
+    he_calculate_net_change_in_disease_stage_count(
+      test_disease_stage_duration_matrices,
+      test_n_newly_infected,
+      test_clinically_infected_prop
+    )
+  expected_net_change <- cbind(-1000, # susceptible
+                               1000 - 50, # latent
+                               11 - 10, # subclinical
+                               39 - 5, # clinical
+                               10, # recovered
+                               5) # dead
+  expect_equal(test_net_change, expected_net_change)
+})
+
 test_that("net change values are calculated correctly for multiple netpens", {
   test_disease_stage_duration_matrices <- list(
     # latent
