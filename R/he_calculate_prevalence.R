77b1408a29e45724109013a1a727e93a465a0321
rachelwoodside <rachel.woodside3@gmail.com>
2025-11-27 16:53:03 -0400

    Clarifying prevalence calculation parameters
    WIP refactoring prevalence and infection probability funcs

--- R/he_calculate_prevalence.R
+++ R/he_calculate_prevalence.R
@@ -1,9 +1,5 @@
-he_calculate_prevalence <- function(infected_netpen_info) {
-  n_infectious <- infected_netpen_info$n_subclinical +
-    infected_netpen_info$n_clinical
-  n_susceptible_or_infected <- infected_netpen_info$n_total -
-    (infected_netpen_info$n_recovered + infected_netpen_info$n_dead)
-  prevalence <- n_infectious / n_susceptible_or_infected
+he_calculate_prevalence <- function(n_infectious, n_not_immune) {
+  prevalence <- n_infectious / n_not_immune
   prevalence[is.nan(prevalence)] <- 0
   prevalence
 }
--- tests/testthat/test-he_calculate_prevalence.R
+++ tests/testthat/test-he_calculate_prevalence.R
@@ -1,32 +1,30 @@
 test_that("prevalence is zero when no animals are infectious", {
-  test_infected_netpen_info <- readRDS(paste0(
-    test_data_filepath,
-    "/infected_netpen_info_dummy_data_latent_only.rds"
-  ))
+  test_n_infectious <- c(0, 0, 0)
+  test_n_not_immune <- c(25000, 25000, 25000)
   test_prevalence <-
-    he_calculate_prevalence(test_infected_netpen_info)
+    he_calculate_prevalence(test_n_infectious, test_n_not_immune)
   expect_equal(test_prevalence, c(0, 0, 0))
 })
 
 test_that("prevalence is zero for netpens where all animals are either recovered or dead and calculated correctly for other netpens", {
-  test_infected_netpen_info <-
-    readRDS(paste0(
-      test_data_filepath,
-      "/infected_netpen_info_dummy_data_with_infection_and_resolved_netpen.rds"
-    ))
+  test_n_infectious <- c(0, 15000, 15000)
+  test_n_not_immune <- c(
+    25000 - 10000 - 15000,
+    25000 - 2000 - 2000,
+    25000 - 1500 - 15000
+  )
   test_prevalence <-
-    he_calculate_prevalence(test_infected_netpen_info)
-  expect_equal(test_prevalence, c(0, 15000 / 21000, 15000 / 22000))
+    he_calculate_prevalence(test_n_infectious, test_n_not_immune)
+  expect_equal(
+    test_prevalence,
+    c(0, 15000 / (25000 - 2000 - 2000), 15000 / (25000 - 1500 - 15000))
+  )
 })
 
 test_that("prevalence is calculated correctly for a single infected netpen with infectious animals and non-immune animals", {
-  test_infected_netpen_info <-
-    readRDS(paste0(
-      test_data_filepath,
-      "/infected_netpen_info_dummy_data_single_row_with_infection.rds"
-    ))
+  test_n_infectious <- 2500
+  test_n_not_immune <- 25000
   test_prevalence <-
-    he_calculate_prevalence(test_infected_netpen_info)
-  expected_prevalence <- 2500 / 25000
-  expect_equal(test_prevalence, expected_prevalence)
+    he_calculate_prevalence(test_n_infectious, test_n_not_immune)
+  expect_equal(test_prevalence, 2500 / 25000)
 })
