84da717e41dd4b121855e594bb4016ac565133de
Rachel Woodside <rachel.woodside3@gmail.com>
2026-01-05 12:57:30 -0400

    Update net pen info reading tests to use helper vars

--- tests/testthat/test-he_read_net_pen_info_file.R
+++ tests/testthat/test-he_read_net_pen_info_file.R
@@ -1,8 +1,5 @@
 test_that("net pen info file reads in with correct values", {
-  filepath <- system.file("testdata", package = "hydroepixr")
-
-  dat <- he_read_net_pen_info_file(paste0(filepath, "/net_pen_file_bay_x.csv"))
-
+  dat <- he_read_net_pen_info_file(test_net_pen_info_filepath)
   expect_no_error(dat)
   expected_dat <- readRDS(paste0(filepath, "/raw_net_pen_info_bay_x.rds"))
   mismatched_values <- which(dat != expected_dat)
@@ -10,10 +7,9 @@ test_that("net pen info file reads in with correct values", {
 })
 
 test_that("net pen info file without correct headers throws error", {
-  filepath <- system.file("testdata", package = "hydroepixr")
   expect_error(
     he_read_net_pen_info_file(paste0(
-      filepath,
+      test_data_filepath,
       "/net_pen_file_bay_x_wrong_headers.csv"
     )),
     regexp = "Unexpected column headers. Expected headers are: "
@@ -21,10 +17,9 @@ test_that("net pen info file without correct headers throws error", {
 })
 
 test_that("net pen info file with duplicate net pen identifiers throws error", {
-  filepath <- system.file("testdata", package = "hydroepixr")
   expect_error(
     he_read_net_pen_info_file(paste0(
-      filepath,
+      test_data_filepath,
       "/net_pen_file_bay_x_duplicate_net_pen_ids.csv"
     )),
     regexp = "Net pen ID numbers are not unique. Simulations Fails. Duplicate Value: "
