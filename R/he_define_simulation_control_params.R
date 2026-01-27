4a5f02e25cd68af2b66ac8dfda59a08c5b606498
Rachel Woodside <rachel.woodside3@gmail.com>
2026-01-27 17:27:53 -0400

    Input guardrails and tests for simulation control vars

--- R/he_define_simulation_control_params.R
+++ R/he_define_simulation_control_params.R
@@ -13,12 +13,29 @@
 #' @export
 
 he_define_simulation_control_params <-
-  function(model_env,
-           n_simulations = 10,
-           max_outbreak_length = 365,
-           random_seed = -10,
-           ignore_disease_status_input = TRUE,
-           verbose = FALSE) {
+  function(
+    model_env,
+    n_simulations = 10,
+    max_outbreak_length = 365,
+    random_seed = -10,
+    ignore_disease_status_input = TRUE,
+    verbose = FALSE
+  ) {
+    if (n_simulations < 10) {
+      warning(
+        "Due to the inclusion of randomness in the model, running a small number of simulations will provide a less representative model. It is recommended to run several simulations and aggregate their results."
+      )
+    }
+    if (n_simulations > 1000) {
+      warning(
+        "Running a large number of simulations will take a large amount of time. It is recommended not to run more than 1000 simulations."
+      )
+    }
+    if (max_outbreak_length > 720) {
+      warning(
+        "Simulations with a long max_outbreak_length will take a large amount of time to run and will not typically represent the length of time the animals will be in the water. Values of 720 days or less are recommended."
+      )
+    }
     model_env$n_simulations <- n_simulations
     model_env$max_outbreak_length <- max_outbreak_length
     model_env$random_seed <- random_seed
--- tests/testthat/test-he_define_simulation_control_params.R
+++ tests/testthat/test-he_define_simulation_control_params.R
@@ -1,3 +1,37 @@
+test_that("a value of n_simulation less than 10 generates a warning", {
+  test_environment <- rlang::new_environment()
+  expect_warning(
+    he_define_simulation_control_params(
+      test_environment,
+      n_simulations = 1
+    ),
+    regexp = "Due to the inclusion of randomness in the model, running a small number of simulations will provide a less representative model. It is recommended to run several simulations and aggregate their results."
+  )
+})
+
+
+test_that("a value of n_simulation greater than 1000 generates a warning", {
+  test_environment <- rlang::new_environment()
+  expect_warning(
+    he_define_simulation_control_params(
+      test_environment,
+      n_simulations = 2000
+    ),
+    regexp = "Running a large number of simulations will take a large amount of time. It is recommended not to run more than 1000 simulations."
+  )
+})
+
+test_that("a value of max_outbreak_length greater than 720 generates a warning", {
+  test_environment <- rlang::new_environment()
+  expect_warning(
+    he_define_simulation_control_params(
+      test_environment,
+      max_outbreak_length = 1000
+    ),
+    regexp = "Simulations with a long max_outbreak_length will take a large amount of time to run and will not typically represent the length of time the animals will be in the water. Values of 720 days or less are recommended."
+  )
+})
+
 test_that("default simulation control parameters are stored in the environment", {
   test_environment <- rlang::new_environment()
 
@@ -11,23 +45,28 @@ test_that("default simulation control parameters are stored in the environment",
 })
 
 test_that("user-defined simulation control parameters stored in the environment", {
-  test_n_simulations <- 1
-  test_max_outbreak_length <- 730
+  test_n_simulations <- 100
+  test_max_outbreak_length <- 720
   test_random_seed <- 123987234
   test_ignore_disease_status_input <- FALSE
   test_verbose <- TRUE
 
   test_environment <- rlang::new_environment()
-  he_define_simulation_control_params(test_environment,
-                                      test_n_simulations,
-                                      test_max_outbreak_length,
-                                      test_random_seed,
-                                      test_ignore_disease_status_input,
-                                      test_verbose)
+  he_define_simulation_control_params(
+    test_environment,
+    test_n_simulations,
+    test_max_outbreak_length,
+    test_random_seed,
+    test_ignore_disease_status_input,
+    test_verbose
+  )
 
   expect_equal(test_environment$n_simulations, test_n_simulations)
   expect_equal(test_environment$max_outbreak_length, test_max_outbreak_length)
   expect_equal(test_environment$random_seed, test_random_seed)
-  expect_equal(test_environment$ignore_disease_status_input, test_ignore_disease_status_input)
+  expect_equal(
+    test_environment$ignore_disease_status_input,
+    test_ignore_disease_status_input
+  )
   expect_equal(test_environment$verbose, test_verbose)
 })
