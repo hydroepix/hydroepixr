dd92e46decdb535ce5fb615410e0fa256e5d8c12
Rachel Woodside <rachel.woodside3@gmail.com>
2026-01-28 17:28:16 -0400

    Type checks and tests for simulation control params

--- R/he_define_simulation_control_params.R
+++ R/he_define_simulation_control_params.R
@@ -22,6 +22,10 @@ he_define_simulation_control_params <-
     verbose = FALSE
   ) {
     # Input checks
+    # n_simulations
+    if (!is.numeric(n_simulations) | n_simulations < 1) {
+      stop("Error: n_simulations value must be a numeric value greater than 0")
+    }
     if (n_simulations < 10) {
       warning(
         "Warning: Due to the inclusion of randomness in the model, running a small number of simulations will provide a less representative model. It is recommended to run several simulations and aggregate their results."
@@ -41,6 +45,24 @@ he_define_simulation_control_params <-
         "Warning: Simulations with a long max_outbreak_length will take a large amount of time to run and will not typically represent the length of time the animals will be in the water. Values of 720 days or less are recommended."
       )
     }
+    # random_seed
+    if (!is.numeric(random_seed)) {
+      stop("Error: Random seed must be a numeric value")
+    }
+    # ignore_disease_status_input
+    if (
+      !isTRUE(ignore_disease_status_input) &
+        !isFALSE(ignore_disease_status_input)
+    ) {
+      stop(
+        "Error: ignore_disease_status should be a boolean value of either TRUE or FALSE"
+      )
+    }
+    # verbose
+    if (!isTRUE(verbose) & !isFALSE(verbose)) {
+      stop("Error: verbose should be a boolean value (either TRUE or FALSE)")
+    }
+
     model_env$n_simulations <- n_simulations
     model_env$max_outbreak_length <- max_outbreak_length
     model_env$random_seed <- random_seed
--- tests/testthat/test-he_define_simulation_control_params.R
+++ tests/testthat/test-he_define_simulation_control_params.R
@@ -1,4 +1,26 @@
-test_that("a value of n_simulation less than 10 generates a warning", {
+test_that("a non-numeric n_simulations value generates an error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      n_simulations = 0
+    ),
+    regexp = "Error: n_simulations value must be a numeric value greater than 0"
+  )
+})
+
+test_that("a value of n_simulations less than 1 generates an error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      n_simulations = "ten"
+    ),
+    regexp = "Error: n_simulations value must be a numeric value greater than 0"
+  )
+})
+
+test_that("a value of n_simulations less than 10 generates a warning", {
   test_environment <- rlang::new_environment()
   expect_warning(
     he_define_simulation_control_params(
@@ -9,8 +31,7 @@ test_that("a value of n_simulation less than 10 generates a warning", {
   )
 })
 
-
-test_that("a value of n_simulation greater than 1000 generates a warning", {
+test_that("a value of n_simulations greater than 1000 generates a warning", {
   test_environment <- rlang::new_environment()
   expect_warning(
     he_define_simulation_control_params(
@@ -18,6 +39,28 @@ test_that("a value of n_simulation greater than 1000 generates a warning", {
       n_simulations = 2000
     ),
     regexp = "Running a large number of simulations will take a large amount of time. It is recommended not to run more than 1000 simulations."
+  )
+})
+
+test_that("a non-numeric max_outbreak_length value generates an error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      max_outbreak_length = "ten"
+    ),
+    regexp = "Error: max_outbreak_length must be numeric value greater than 0"
+  )
+})
+
+test_that("a value of max_outbreak_length less than 1 generates an error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      max_outbreak_length = 0
+    ),
+    regexp = "Error: max_outbreak_length must be numeric value greater than 0"
   )
 })
 
@@ -29,6 +72,36 @@ test_that("a value of max_outbreak_length greater than 720 generates a warning",
       max_outbreak_length = 1000
     ),
     regexp = "Simulations with a long max_outbreak_length will take a large amount of time to run and will not typically represent the length of time the animals will be in the water. Values of 720 days or less are recommended."
+  )
+})
+
+test_that("a non-numeric random_seed value generates an error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      random_seed = "random"
+    )
+  )
+})
+
+test_that("non boolean value for ignore_disease_status_input generates error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control_params(
+      test_environment,
+      ignore_disease_status_input = "false"
+    )
+  )
+})
+
+test_that("non boolean value for verbose generates error", {
+  test_environment <- rlang::new_environment()
+  expect_error(
+    he_define_simulation_control(
+      test_environment,
+      verbose = 1
+    )
   )
 })
 
