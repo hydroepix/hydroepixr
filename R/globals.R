b487052c0e2ae0c6fa801c8ab4431f2a3c094d51
Danielle Dempsey <ddempsey@perennia.ca>
2026-02-23 13:49:52 -0400

    initial commit

---
+++ R/globals.R
@@ -0,0 +1,14 @@
+utils::globalVariables(
+  c(
+    # he_initialize_netpen_info
+    'within_netpen_transmission_min',
+    'within_netpen_transmission_mode',
+    'within_netpen_transmission_max',
+
+    # he_plot_simulation
+    'infection_stage',
+
+    'simulation_day',
+    'value'
+  )
+)
