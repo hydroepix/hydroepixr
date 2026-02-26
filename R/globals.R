646de4cc5bc37fcd8145cd6c478ea8a344a87bd2
Rachel Woodside <rachel.woodside3@gmail.com>
2026-02-26 10:31:32 -0400

    Update globals.R

--- R/globals.R
+++ R/globals.R
@@ -1,14 +1,25 @@
 utils::globalVariables(
   c(
-    # he_initialize_netpen_info
-    'within_netpen_transmission_min',
-    'within_netpen_transmission_mode',
-    'within_netpen_transmission_max',
-
-    # he_plot_simulation
+    # expected columns in data frames
+    'within_net_pen_transmission_min',
+    'within_net_pen_transmission_mode',
+    'within_net_pen_transmission_max',
+    "species_id",
     'infection_stage',
+    "farm_id",
+    "n_susceptible",
+    "n_latent",
+    "n_subclinical",
+    "n_clinical",
+    "n_recovered",
+    "n_dead",
+    "simulation_num",
+    "net_pen_id",
 
+    # expected variables from another environment
     'simulation_day',
+
+    # plotting
     'value'
   )
 )
